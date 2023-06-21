4c87f67aef17001994cb14efdf21ac240edc8eb5
SamShum_June <38761558+SamShum-June@users.noreply.github.com>
2023-06-21 15:03:20 +0800

    1

--- first_Python.py
+++ first_Python.py
@@ -1,8 +1,18 @@
-name = "samshum,"
-first = "Hello,"
-second = "My World!"
-print("Hello " + name);
+# name = "samshum,"
+# first = "Hello,"
+# second = "My World!"
+# print("Hello " + name);
 
-print(name + first + second)
+# print(name + first + second)
 
-print()
+# print()
+
+
+
+
+chenji = 100
+raing = True
+if chenji==100 and raing:
+    print("I give you 1000 yuan")
+else:
+    print("you give me 100 yuan")
